26d05a73d0e61e09dda03d5bc2868bc2e727df54
lsljw <lsljw@naver.com>
2019-03-05 16:40:56 +0900

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
 $(function(){
+	var windowWidth=$(window).width();
+	var sWidth=$(".s-button").width();
+	var ws=(windowWidth-sWidth)/2;
+	$(".s-button").css({
+		left:ws
+	});
+	
+	$(window).resize(function(){
+		var windowWidth=$(window).width();
+		var sWidth=$(".s-button").width();
+		var ws=(windowWidth-sWidth)/2;
+		$(".s-button").css({
+			left:ws
+		});
+	});
+	
+	//스크롤 버튼 애니메이션
 	
 	
+	setInterval(function(){
+		$(".s-button").animate({
+		bottom:30
+		});
+		$(".s-button").animate({
+			bottom:20
+		});
+	});
 	
 });
