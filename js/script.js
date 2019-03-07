ccc03247d78193b73b96326bfa3ce005f1207190
lsljw <lsljw@naver.com>
2019-03-07 17:04:43 +0900

    update 3

--- js/script.js
+++ js/script.js
@@ -27,4 +27,31 @@ $(function(){
 		});
 	});
 	
+	console.log("섹션0 : "+$(".s0").offset().top);
+	console.log("섹션1 : "+$(".s1").offset().top);
+	
+	$(window).scroll(function(){
+		var sTop=$(window).scrollTop();
+		if($(".s0").offset().top<sTop+700){
+			
+			$(".s0").css({opacity:1});
+		}
+		if($(".s1").offset().top<sTop+700){
+			
+			$(".s1").css({opacity:1});
+		}
+		if($(".s2").offset().top<sTop+700){
+			
+			$(".s2").css({opacity:1});
+		}
+		if($(".s3").offset().top<sTop+700){
+			
+			$(".s3").css({opacity:1});
+		}
+		if($(".s4").offset().top<sTop+700){
+			
+			$(".s4").css({opacity:1});
+		}
+	});
+
 });
